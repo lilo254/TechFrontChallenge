5602f0f235255ff7ac659ecb9b71779153c2743b
Lillian <lilichangeiywo@gmail.com>
2018-07-09 22:51:09 +0300

    add

---
+++ changeiywoLillian.js
@@ -0,0 +1,29 @@
+class Darasa {
+  constructor(name) {['Jeff', 'Linda', 'Grace', 'Mike', 'Matt', 'Juliet']
+  }
+}
+
+
+def addClassMember(name) {
+  if (var i=0; i<name.length; i++) {
+    return "Darasa member added";
+  } else {
+    return "Member exists";
+  }
+}
+
+console.log(addClassMember(name));
+
+
+def removeClassMember(name) {
+  if (var i=0; i>name.length; i++) {
+    return "Darasa member removed from class";
+  } else {
+    return "Member does not exists";
+  }
+}
+
+console.log(removeClassMember(name));
+
+def getDarasaMembers(name)
+console.log(getDarasaMembers(name));
